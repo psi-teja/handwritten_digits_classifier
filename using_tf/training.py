9dd789b2bbf5663872e1a7f8497a0520b6b8081f
teja <psi.teja@gmail.com>
2023-09-02 21:01:50 +0530

    changes in training.py

--- using_tf/training.py
+++ using_tf/training.py
@@ -1,7 +1,7 @@
 from dataset import *
-from tensorflow.keras import models
+from model import model
 
-tf_model = models.load_model('tf_model')
+tf_model = model
 
 tf_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 # tf_model.optimizer.lr.assign(0.005)
