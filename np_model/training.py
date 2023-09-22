5282291fa10c514c66d85cd7867573ceed26cc03
Teja <psi.teja@gmail.com>
2023-09-22 19:25:41 +0530

    changed np_model

--- np_model/training.py
+++ np_model/training.py
@@ -2,21 +2,22 @@ import numpy as np
 import pickle as pkl
 from tqdm import trange
 from keras.datasets import mnist
-from utils import preprocess_data, sigmoid, softmax, d_sigmoid
+from utils import preprocess_data_for_training, sigmoid, softmax, d_sigmoid
 
 
 # loading MNIST dataset
 (X_train, y_train), (_, _) = mnist.load_data()
 
 N = len(X_train)
-epochs = 1000
-batch_size = 64
-initial_learning_rate = 0.001
+epochs = 100
+batch_size = 1000
+learning_rate = 0.001
+lamda_l2 = 0.002
 
 # designing MLPs
 n_in = 784
 n_hl1 = 128
-n_hl2 = 64
+n_hl2 = 128
 n_out = 10
 
 # Random initialization of weights and biases
@@ -34,7 +35,7 @@ bo = 2 * np.random.random((1, n_out)) - 1
 #     w1, w2, wo, b1, b2, bo = weights
 
 # preprocessing training data
-X_train, Y_train = preprocess_data(X_train, y_train)
+X_train, Y_train = preprocess_data_for_training(X_train, y_train)
 
 epoch_list = []
 
@@ -71,25 +72,25 @@ for j in trange(epochs, desc="Training In Progress", unit="epochs"):
         Error_at_hl1 = np.dot(d_hl2, w2.transpose())
         d_hl1 = Error_at_hl1 * slope_hl1
 
-        grad_wo = np.dot(hl2_a.transpose(), d_out)   
-        grad_w2 = np.dot(hl1_a.transpose(), d_hl2)  
-        grad_w1 = np.dot(X_batch.transpose(), d_hl1) 
+        grad_wo = np.dot(hl2_a.transpose(), d_out)   - 2*lamda_l2*wo
+        grad_w2 = np.dot(hl1_a.transpose(), d_hl2)   - 2*lamda_l2*w2
+        grad_w1 = np.dot(X_batch.transpose(), d_hl1) - 2*lamda_l2*w1
 
-        grad_bo = d_out.sum(axis=0) 
-        grad_b2 = d_hl2.sum(axis=0) 
-        grad_b1 = d_hl1.sum(axis=0) 
+        grad_bo = d_out.sum(axis=0)   - 2*lamda_l2*bo
+        grad_b2 = d_hl2.sum(axis=0)   - 2*lamda_l2*b2
+        grad_b1 = d_hl1.sum(axis=0)   - 2*lamda_l2*b1
 
-        wo += grad_wo * (initial_learning_rate)
-        w2 += grad_w2 * (initial_learning_rate)
-        w1 += grad_w1 * (initial_learning_rate)
+        wo += grad_wo * learning_rate
+        w2 += grad_w2 * learning_rate
+        w1 += grad_w1 * learning_rate
 
-        bo += grad_bo * (initial_learning_rate)
-        b2 += grad_b2 * (initial_learning_rate)
-        b1 += grad_b1 * (initial_learning_rate)
+        bo += grad_bo * learning_rate
+        b2 += grad_b2 * learning_rate
+        b1 += grad_b1 * learning_rate
 
         i += batch_size
     print(error)
 
 # Save the trained weights and biases
 weights = [w1, w2, wo, b1, b2, bo]
-pkl.dump(weights, open('weights/model_weights.pkl', 'wb'))
+pkl.dump(weights, open('np_model/weights/model_weights.pkl', 'wb'))
